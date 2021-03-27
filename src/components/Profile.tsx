65e19138ce1cb5bbf747437cf59f80e274c8e204
wesferoli <wesleyfer.oli97@gmail.com>
2021-03-27 09:12:07 -0300

    First commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/augustaraujo.png" alt="Wesley Ferreira" />
+      <div>
+        <strong>Augusto Araújo</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level { level }
+        </p>
+      </div>
+    </div>
+  );
+}
